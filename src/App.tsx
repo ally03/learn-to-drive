89811c6fc8618e34ece5df33e2025264d92fe0a5
alid didarali <aliddidarali02@gmail.com>
2020-03-17 16:36:59 +0000

    made an post request on submit button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import LoginForm from "./loginComponet/LoginForm";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
   render() {
