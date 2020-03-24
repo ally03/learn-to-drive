48db62817b49b856a5e9322c8270c4a88982b31f
alid didarali <aliddidarali02@gmail.com>
2020-03-24 14:25:57 +0000

    added alert text if the postcode is not valid

--- src/Pages/search/HomeSearch.tsx
+++ src/Pages/search/HomeSearch.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link, Redirect } from "react-router-dom";
-import { Form, Button, Card, Row, Col } from "react-bootstrap";
+import { Form, Button, Card, Row, Col, Alert } from "react-bootstrap";
 import "./homesearch.css";
 
 interface HomeSearchState {
   postCode: string;
   validSearch: boolean;
+  alert: string;
 }
 
 interface Props {}
@@ -15,7 +16,8 @@ class HomeSearch extends React.Component<Props, HomeSearchState> {
     super(props);
     this.state = {
       postCode: "",
-      validSearch: false
+      validSearch: false,
+      alert: ""
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
@@ -24,7 +26,6 @@ class HomeSearch extends React.Component<Props, HomeSearchState> {
     this.setState({
       postCode: e.target.value
     });
-    console.log("postcode", this.state);
   }
 
   handleSearch(e: any) {
@@ -32,7 +33,6 @@ class HomeSearch extends React.Component<Props, HomeSearchState> {
     this.setState({
       postCode: this.state.postCode
     });
-    console.log("postcodeSearch", this.state);
     this.postcodeAPI();
   }
   postcodeAPI = async () => {
@@ -49,6 +49,11 @@ class HomeSearch extends React.Component<Props, HomeSearchState> {
     console.log("response", response.status);
     if (response.status === 200) {
       this.setState({ validSearch: true });
+    } else if (response.status === 400) {
+      this.setState({
+        alert: "Please enter a valid post code"
+      });
+      console.log("alret", this.state);
     }
     // return await response;
   };
@@ -68,9 +73,8 @@ class HomeSearch extends React.Component<Props, HomeSearchState> {
               <p className="searchText">
                 Find most humble and experienced instructors near you.
               </p>
-
               <Row>
-                <Col className="searchbox" onClick={this.handleSearch}>
+                <Col className="searchbox">
                   <Form.Control
                     size="lg"
                     type="search"
@@ -78,11 +82,17 @@ class HomeSearch extends React.Component<Props, HomeSearchState> {
                     value={this.state.postCode}
                     onChange={this.handleChange}
                   />
-                  <Button size="lg" variant="primary" type="submit">
+                  <Button
+                    size="lg"
+                    variant="primary"
+                    type="submit"
+                    onClick={this.handleSearch}
+                  >
                     Search
                   </Button>
                 </Col>
               </Row>
+              <p className="searchAlert">{this.state.alert}</p>
             </Form>
           </Card.Body>
         </Card>
